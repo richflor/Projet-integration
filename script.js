87b9abb386c1c7e50de328886602f34c9e113d76
richflor <fld.richard@gmail.com>
2022-02-20 19:16:26 +0100

    fix commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let notif = document.getElementById('notif');
+// let labelNotif = document.getElementById('btn-notif');
+
+// function toggleNotif() {
+//     labelNotif.addEventListener('click', function (event) () => {
+//         notif.classList.toggle("notif-false")
+//     }
+// }
